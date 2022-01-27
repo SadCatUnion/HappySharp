cb872c9d8af07c5e0c424500ad2e2a1de231e402
Fungus-Light <542461670@qq.com>
2022-01-27 11:20:23 +0800

    a good start
    add console to the jsenv

---
+++ HappySharp/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.ClearScript.V8;
+using Serilog;
+
+namespace HappySharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Console.WriteLine("You are now running HappySharp......");
+            var jsEnv = new V8ScriptEngine();
+            jsEnv.AddHostObject("logger",logger);
+            jsEnv.AddHostType("Console",typeof(Console));
+            Console.WriteLine();
+            if(File.Exists("./js/log.js")){
+                var logjsTxt = File.ReadAllText("./js/log.js");
+                jsEnv.Execute(logjsTxt);
+            }else{
+                logger.Error("Could Not Find ./js/log.js");
+            }
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # HappySharp
 The combination of Ts and C# makes the program development process happier
+
+- Install .NET6 SDK
+- Clone this repo
+- Enter the **HappySharp** Directory
+- **dotnet run**
