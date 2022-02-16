9ebc5276cf457cebf3f87a0ad6f935addaffd0cf
Fungus-Light <542461670@qq.com>
2022-02-16 17:42:13 +0800

    add much types to Bind

--- HappySharpProj/Assets/Scripts/Editor/BindConfig.cs
+++ HappySharpProj/Assets/Scripts/Editor/BindConfig.cs
@@ -1,33 +1,28 @@
-﻿using System.IO;
-using System.Collections.Generic;
-using UnityEngine;
-using Puerts;
+﻿using Puerts;
 using System;
-using UnityEngine.Playables;
-using UnityEngine.UI;
-using UnityEngine.Events;
-using UnityEngine.EventSystems;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
 
+/// <summary>
+/// 如果你全ts/js编程，可以参考这份自动化配置
+/// </summary>
 [Configure]
-public class BindConfig
+public class PuertsConfig
 {
-    [Binding]
-    static IEnumerable<Type> Bindings
+    [Typing]
+    static IEnumerable<Type> Typeing
     {
         get
         {
             return new List<Type>()
             {
-                typeof(Application),
-                typeof(Entrance),
-                typeof(Environment),
-                typeof(GUI),
-                typeof(Rect),
-                typeof(Screen)
+                //仅生成ts接口, 不生成C#静态代码
+                //typeof(Dictionary<int,int>)
             };
         }
     }
-
     [BlittableCopy]
     static IEnumerable<Type> Blittables
     {
@@ -40,14 +35,154 @@ public class BindConfig
             };
         }
     }
-
-    [Filter]
-    static bool Filter(System.Reflection.MemberInfo memberInfo)
+    static IEnumerable<Type> Bindings
     {
-        return (memberInfo.DeclaringType.Name == "Flowchart" && memberInfo.Name == "SelectedCommandsStale") ||
-        (memberInfo.DeclaringType.Name == "Input" && memberInfo.Name == "IsJoystickPreconfigured")||
-        (memberInfo.DeclaringType.Name == "MonoBehaviour" && memberInfo.Name == "runInEditMode")||
-        (memberInfo.DeclaringType.Name == "Text" && memberInfo.Name == "OnRebuildRequested");
+        get
+        {
+            return new List<Type>()
+            {
+               //直接指定的类型
+                typeof(JsEnv),
+                typeof(ILoader),
+            };
+        }
     }
+    [Binding]
+    static IEnumerable<Type> DynamicBindings
+    {
+        get
+        {
+            // 在这里添加名字空间
+            var namespaces = new List<string>()
+            {
+                "UnityEngine",
+                "UnityEngine.UI",
+                "System",
+                "System.IO",
+                "System.Security",
+                "System.Security.Policy"
+            };
+            var unityTypes = (from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                              where !(assembly.ManifestModule is System.Reflection.Emit.ModuleBuilder)
+                              from type in assembly.GetExportedTypes()
+                              where type.Namespace != null && namespaces.Contains(type.Namespace) && !IsExcluded(type)
+                              select type);
+            string[] customAssemblys = new string[] {
+                "Assembly-CSharp",
+            };
+            var customTypes = (from assembly in customAssemblys.Select(s => Assembly.Load(s))
+                               where !(assembly.ManifestModule is System.Reflection.Emit.ModuleBuilder)
+                               from type in assembly.GetExportedTypes()
+                               where type.Namespace == null || !type.Namespace.StartsWith("Puerts")
+                                    && !IsExcluded(type)
+                               select type);
+            return unityTypes
+                .Concat(customTypes)
+                .Concat(Bindings)
+                .Distinct();
+        }
+    }
+    static bool IsExcluded(Type type)
+    {
+        if (type == null)
+            return false;
 
+        string assemblyName = Path.GetFileName(type.Assembly.Location);
+        if (excludeAssemblys.Contains(assemblyName))
+            return true;
+
+        string fullname = type.FullName != null ? type.FullName.Replace("+", ".") : "";
+        if (excludeTypes.Contains(fullname))
+            return true;
+        return IsExcluded(type.BaseType);
+    }
+    //需要排除的程序集
+    static List<string> excludeAssemblys = new List<string>{
+        "UnityEditor.dll",
+        "Assembly-CSharp-Editor.dll",
+    };
+    //需要排除的类型
+    static List<string> excludeTypes = new List<string>
+    {
+        "UnityEngine.iPhone",
+        "UnityEngine.iPhoneTouch",
+        "UnityEngine.iPhoneKeyboard",
+        "UnityEngine.iPhoneInput",
+        "UnityEngine.iPhoneAccelerationEvent",
+        "UnityEngine.iPhoneUtils",
+        "UnityEngine.iPhoneSettings",
+        "UnityEngine.AndroidInput",
+        "UnityEngine.AndroidJavaProxy",
+        "UnityEngine.BitStream",
+        "UnityEngine.ADBannerView",
+        "UnityEngine.ADInterstitialAd",
+        "UnityEngine.RemoteNotification",
+        "UnityEngine.LocalNotification",
+        "UnityEngine.NotificationServices",
+        "UnityEngine.MasterServer",
+        "UnityEngine.Network",
+        "UnityEngine.NetworkView",
+        "UnityEngine.ParticleSystemRenderer",
+        "UnityEngine.ParticleSystem.CollisionEvent",
+        "UnityEngine.ProceduralPropertyDescription",
+        "UnityEngine.ProceduralTexture",
+        "UnityEngine.ProceduralMaterial",
+        "UnityEngine.ProceduralSystemRenderer",
+        "UnityEngine.TerrainData",
+        "UnityEngine.HostData",
+        "UnityEngine.RPC",
+        "UnityEngine.AnimationInfo",
+        "UnityEngine.UI.IMask",
+        "UnityEngine.Caching",
+        "UnityEngine.Handheld",
+        "UnityEngine.MeshRenderer",
+        "UnityEngine.UI.DefaultControls",
+        "UnityEngine.AnimationClipPair", //Obsolete
+        "UnityEngine.CacheIndex", //Obsolete
+        "UnityEngine.SerializePrivateVariables", //Obsolete
+        "UnityEngine.Networking.NetworkTransport", //Obsolete
+        "UnityEngine.Networking.ChannelQOS", //Obsolete
+        "UnityEngine.Networking.ConnectionConfig", //Obsolete
+        "UnityEngine.Networking.HostTopology", //Obsolete
+        "UnityEngine.Networking.GlobalConfig", //Obsolete
+        "UnityEngine.Networking.ConnectionSimulatorConfig", //Obsolete
+        "UnityEngine.Networking.DownloadHandlerMovieTexture", //Obsolete
+        "AssetModificationProcessor", //Obsolete
+        "AddressablesPlayerBuildProcessor", //Obsolete
+        "UnityEngine.WWW", //Obsolete
+        "UnityEngine.EventSystems.TouchInputModule", //Obsolete
+        "UnityEngine.MovieTexture", //Obsolete[ERROR]
+        "UnityEngine.NetworkPlayer", //Obsolete[ERROR]
+        "UnityEngine.NetworkViewID", //Obsolete[ERROR]
+        "UnityEngine.NetworkMessageInfo", //Obsolete[ERROR]
+        "UnityEngine.UI.BaseVertexEffect", //Obsolete[ERROR]
+        "UnityEngine.UI.IVertexModifier", //Obsolete[ERROR]
+        //Windows Obsolete[ERROR]
+        "UnityEngine.EventProvider",
+        "UnityEngine.UI.GraphicRebuildTracker",
+        "UnityEngine.GUI.GroupScope",
+        "UnityEngine.GUI.ScrollViewScope",
+        "UnityEngine.GUI.ClipScope",
+        "UnityEngine.GUILayout.HorizontalScope",
+        "UnityEngine.GUILayout.VerticalScope",
+        "UnityEngine.GUILayout.AreaScope",
+        "UnityEngine.GUILayout.ScrollViewScope",
+        "UnityEngine.GUIElement",
+        "UnityEngine.GUILayer",
+        "UnityEngine.GUIText",
+        "UnityEngine.GUITexture",
+        "UnityEngine.ClusterInput",
+        "UnityEngine.ClusterNetwork",
+        //System
+        "System.Tuple",
+        "System.Double",
+        "System.Single",
+        "System.ArgIterator",
+        "System.SpanExtensions",
+        "System.TypedReference",
+        "System.StringBuilderExt",
+        "System.IO.Stream",
+        "System.Net.HttpListenerTimeoutManager",
+        "System.Net.Sockets.SocketAsyncEventArgs",
+    };
 }
